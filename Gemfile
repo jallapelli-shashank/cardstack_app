8d8a3ff088c10071ec450b8a1d74d973506b011b
Shashank Jallapelli <projects.shashankjallapelli@gmail.com>
2026-06-03 15:48:48 +0530

    implemented changes to determine card type based on card number

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem "image_processing", "~> 1.2"
 
 gem 'vite_rails'
 
+gem 'credit_card_validations'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,53 @@
 class Card < ApplicationRecord
   belongs_to :user
   has_many :bills, dependent: :destroy
+  enum :bank, {
+    axis: "Axis",
+    hdfc: "HDFC",
+    amex: "American Express",
+    hsbc: "HSBC",
+    icici: "ICICI",
+    indusind: "IndusInd"
+  }
 
-  validates :name, :card_type, :bank, :card_number, :expiry_month, :expiry_year, presence: true
-  validates :card_number, uniqueness: true
+  attr_accessor :number
 
-  encrypts :card_number
+  before_validation :extract_card_details
+
+  validates :name, :iin, :last_four_digits, :expiry_month, :expiry_year, :bank, presence: true
+  validates :last_four_digits, length: { is: 4 }
+  validate :number_is_valid, on: :create
+
+  def total_spent
+    bills.sum(:amount)
+  end
+
+  def milestones_string
+    milestones.map(&:)
+    milestones&.join(";") || ""
+  end
+
+  def milestones_string=(value)
+    self.milestones = value.gsub(",", "").split(";").map(&:to_i)
+  end
+
+  private
+
+  def extract_card_details
+    return if number.blank?
+
+    digits = number.gsub(/\D/, "")
+    detector = CreditCardValidations::Detector.new(digits)
+
+    self.card_type = detector.brand
+    self.iin = digits.first(6).to_i
+    self.last_four_digits = digits.last(4)
+  end
+
+  def number_is_valid
+    digits = number.to_s.gsub(/\D/, "")
+    detector = CreditCardValidations::Detector.new(digits)
+
+    errors.add(:number, "Please enter a valid card number") if detector.brand.nil?
+  end
 end
